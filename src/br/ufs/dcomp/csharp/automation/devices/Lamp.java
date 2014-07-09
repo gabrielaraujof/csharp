3b77a80ad369906261f6f0f6652a374754b899f8
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2014-07-09 15:47:45 -0300

    Update the Lamp device implementation, adding support for switch (on and off) and dimmer functions.

--- src/br/ufs/dcomp/csharp/automation/devices/Lamp.java
+++ src/br/ufs/dcomp/csharp/automation/devices/Lamp.java
@@ -3,8 +3,12 @@ package br.ufs.dcomp.csharp.automation.devices;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingDeque;
 
-import br.ufs.dcomp.csharp.automation.Node;
+import br.ufs.dcomp.csharp.automation.Actuator;
+import br.ufs.dcomp.csharp.automation.Device;
 import br.ufs.dcomp.csharp.automation.RoomContainer;
+import br.ufs.dcomp.csharp.automation.Sensor;
+import br.ufs.dcomp.csharp.automation.utils.ActuatorFunction;
+import br.ufs.dcomp.csharp.automation.utils.SensorFunction;
 import br.ufs.dcomp.csharp.communication.ArduinoMessenger;
 
 /**
@@ -12,33 +16,90 @@ import br.ufs.dcomp.csharp.communication.ArduinoMessenger;
  * @author gabriel
  *
  */
-public abstract class Lamp extends Node {
-
-	private RoomContainer room;
+public class Lamp extends Device implements Actuator, Sensor {
 
 	private Queue<String> messageBuffer;
 
-	private final int GET_STATE = 2;
+	public Lamp(String nodeId, String name, RoomContainer room,
+			ArduinoMessenger messenger) {
+		super(nodeId, name, messenger, room);
+		this.messageBuffer = new LinkedBlockingDeque<String>();
+	}
 
-	public enum Switch {
-		ON(1), OFF(0);
+	@Override
+	public void act(ActuatorFunction function, int param) {
+		String message;
 
-		private final int code;
-
-		Switch(int c) {
-			this.code = c;
-		}
-
-		public int getValue() {
-			return code;
+		switch (function) {
+		case SWITCH:
+			if (param != Actuator.ON && param != Actuator.OFF)
+				throw new IllegalArgumentException(
+						"Only Actuator.ON and Actuator.OFF values are allowed.");
+			// TODO Must ensure in advance that room and device's code has only
+			// two
+			// characters length. Otherwise, block the request.
+			message = String.format("%s%s%s%s", this.getRoom().getId(),
+					this.getId(), ActuatorFunction.SWITCH.getValue(), param);
+			deliverCommand(message);
+			break;
+		case DIMMER:
+			if (param < 0 && param >= 256)
+				throw new IllegalArgumentException(
+						"Only a value betweeb 0 and 255 is allowed.");
+			// TODO Must ensure in advance that room and device's code has only
+			// two
+			// characters length. Otherwise, block the request.
+			message = String.format("%s%s%s%s", this.getRoom().getId(),
+					this.getId(), ActuatorFunction.DIMMER.getValue(), param);
+			deliverCommand(message);
+			break;
+		default:
+			throw new UnsupportedOperationException(
+					"Unrecognized function on this device.");
 		}
 	}
 
-	public Lamp(String nodeId, String name, RoomContainer room,
-			ArduinoMessenger messenger) {
-		super(nodeId, name, messenger);
-		this.room = room;
-		this.messageBuffer = new LinkedBlockingDeque<String>();
+	@Override
+	public Object sense(SensorFunction function, int param) {
+		String message;
+
+		switch (function) {
+		case GET_STATE:
+			// TODO Must ensure in advance that room and device's code has only
+			// two
+			// characters length. Otherwise, block the request.
+			message = String.format("%s%s%s", this.getRoom().getId(),
+					this.getId(), SensorFunction.GET_STATE.getValue());
+			deliverCommand(message, this);
+			break;
+		case GET_VALUE:
+			// TODO Must ensure in advance that room and device's code has only
+			// two
+			// characters length. Otherwise, block the request.
+			message = String.format("%s%s%s", this.getRoom().getId(),
+					this.getId(), SensorFunction.GET_VALUE.getValue());
+			deliverCommand(message, this);
+			break;
+		default:
+			throw new UnsupportedOperationException(
+					"Unrecognized function on this device.");
+		}
+
+		try {
+			while (messageBuffer.isEmpty()) {
+				synchronized (messageBuffer) {
+					messageBuffer.wait();
+				}
+			}
+			synchronized (messageBuffer) {
+				// May decode the reply before returning it
+				// return decodeMessage(messageBuffer.poll());
+				return messageBuffer.poll();
+			}
+		} catch (InterruptedException e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
 	}
 
 	@Override
@@ -48,34 +109,4 @@ public abstract class Lamp extends Node {
 			notifyAll();
 		}
 	}
-
-	public void turn(Switch state) {
-		// Cria string usando o protocolo
-		String message = room.getId() + this.getId() + state.getValue();
-		deliverCommand(message, false);
-	}
-
-	public boolean isOn() throws InterruptedException {
-
-		// Cria string usando o protocolo
-		String message = room.getId() + this.getId() + GET_STATE;
-		deliverCommand(message, true);
-
-		while (messageBuffer.isEmpty()) {
-			synchronized (messageBuffer) {
-				messageBuffer.wait();
-			}
-		}
-
-		synchronized (messageBuffer) {
-			return decodeMessage(messageBuffer.poll());
-		}
-	}
-
-	protected boolean decodeMessage(String message) {
-		// TODO Auto-generated method stub
-		System.out.println(message);
-		return false;
-	}
-
 }
